e073a65fa27af3af5f0c77506a485d2796965ff7
Greg <1greg4ever@mail.ru>
2021-11-11 20:55:02 +0200

    Completed method getHolidayPrizes

--- independentWork-1/classWork-1.js
+++ independentWork-1/classWork-1.js
@@ -1,10 +1,6 @@
 //file WorkHere.js
 ///https://codesandbox.io/s/classwork-2-rmp2l?file=/src/WorkHere.js
-
 export const dataProcessingAboutSalary = (f, s) => {
-  
-  
-  
   return +f + +s
 };
 
@@ -12,19 +8,11 @@ export const calculateCashback = (isCashback, sum, cb) => {
 if(isCashback){
   sum=sum*cb;
 }
-
-
 };
 
-export const getHolidaysNotifications = (f, s) => {
-
-
-
-};
+export const getHolidaysNotifications = (f, s) => {};
 
 export const getHolidayPrizes = (nameWorker, bDay, salary) => {
-
-// (f, s, t)
 /* С сервера приходят данные о каждом пользователе, а именно: 
 имя/фамилия, дата рождения и зп. Вы должны написать функцию, которая:
 Сравнивает сегодняшнюю дату с датой рождения сотрудника
@@ -35,28 +23,17 @@ export const getHolidayPrizes = (nameWorker, bDay, salary) => {
  В большинстве случаев получаются некрасивые цифры типа 213, 157 
  итд. Нужно сделать суммы круглыми - 220, 160 итд. Воспроизведите 
  этот алгоритм.  */
-
- /*
-если сегодня дата рожд то начальнику 
-`у ${name} сегодня праздник, его премия составляет ${prem}”
-prem=salary*0.1+prem2
-prem2=(round((day*month)/10))*10
- */
-//дату из строки обработать..
 let today= new Date()
+let birthDate= new Date(bDay)
 let prem
-let prem2
-let workerDay
-let workerMonth
-
-if(today==bDay){
-  prem=salary*0,1
-  workerDay=bDay.getDay();
-  workerMonth=bDay.getMonth();
-  prem2=(Math.round((workerDay*workerMonth)/10))*10
-  prem+=prem2
+let workerDay=birthDate.getDate()
+let workerMonth= birthDate.getMonth()
+if(today.getDate()===workerDay && today.getMonth()===workerMonth){
+  prem=Math.ceil(((salary/10)+(workerDay*(workerMonth+1)))/10)*10
   let strCongrat=`у ${nameWorker} сегодня праздник, его премия составляет ${prem}`
   return strCongrat
 };
-
+return 'Нет премии'
 }
+
+
