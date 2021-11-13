3640bf5135d40d9ddeccb3c8801a81606f70906c
Greg <1greg4ever@mail.ru>
2021-11-13 15:42:07 +0200

    Modified homeWork-2

--- homeWork-2/workHere.js
+++ homeWork-2/workHere.js
@@ -6,11 +6,11 @@ export const sum = (f, s) => {
   // Если спарсились оба, то возвращаем их сумму.
   // Если одно, то только что спарсилось.
   // Если ничего, то 0.
-let a=parseFloat(f)
-let b=parseFloat(s)
-a=isNaN(a)?0:+a
-b=isNaN(b)?0:+b
-return a + b;
+  let a=parseFloat(f)
+  let b=parseFloat(s)
+  a=isNaN(a)?0:+a
+  b=isNaN(b)?0:+b
+  return a + b;
 };
 
 export const showPrice = (price, discount) => {
@@ -18,20 +18,18 @@ export const showPrice = (price, discount) => {
 
   //TODO: Привести цену к виду: 10.00 р.
   // округлять до копеек в сторону магазина
-  let result=Math.ceil(discountedPrice*100)/100+' р.'
-  return result;
+  return (Math.ceil(discountedPrice*1000)/1000).toFixed(2)+' р.';
 };
 
 export const findSuccess = (bankResponse) => {
   //TODO: Проверьте что в строке есть 'Success' без учёта регистра
-  let result=bankResponse.toLowerCase().includes('success')
-  return result?'Yes':'No'
+  return bankResponse.toLowerCase().includes('success')?'Yes':'No'
 };
 
 export const dateToString = (date) => {
   //TODO: Преобразовать дату к Вчера/Сегодня/Завтра или год:месяц:день
   //Cегодня завтра будет вчера, а еще вчера сегодня было завтра - Гуф :)
-  
+
   let givenDate= new Date(date)
   let today= new Date()
   givenDate.setHours(0,0,0,0)
@@ -39,18 +37,15 @@ export const dateToString = (date) => {
   let result
   switch(today-givenDate)
   {
-      case 0:result='Today';break;
-      case 86400000:result='Yesterday';break;
-      case -86400000:result='Tomorrow';break;
+      case 0:return 'Today';break;
+      case 86400000:return 'Yesterday';break;
+      case -86400000:return 'Tomorrow';break;
       default:
-        result=`${givenDate.getFullYear()}:${givenDate.getMonth()+1}:${givenDate.getDate()}`
+        return `${givenDate.getFullYear()}:${givenDate.getMonth()+1}:${givenDate.getDate()}`
   }
-  return result;
 };
 
 export const sort = (data) => {
   //TODO: Отсортируйте массив строк по алфавиту
-  
-  let result=data.sort((a,b)=>a.localeCompare(b))
-  return result
+  return data.sort((a,b)=>a.localeCompare(b))
 };
